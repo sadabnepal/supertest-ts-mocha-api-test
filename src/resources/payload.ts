3daf595d1004e7a1e33e4ea715fca7c5057914e7
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-04-30 05:48:40 +0530

    removed ts payload and added json data

--- src/resources/payload.ts
+++
@@ -1,6 +0,0 @@
-import faker from "@faker-js/faker";
-
-export const createUserPayload = {
-    "name": faker.name.firstName(),
-    "job": faker.name.jobTitle()
-}
--- src/test/Users.spec.ts
+++ src/test/Users.spec.ts
@@ -1,16 +1,16 @@
 import { expect } from 'chai';
-import { createUserPayload } from 'src/resources/payload';
+import userdata from "src/resources/testdata.json";
 import supertest from 'supertest';
 
 const request = supertest("https://reqres.in/api/");
 
 describe('Test ReqRes APIs', () => {
 
-    it('TC_001_should validate create users', async () => {
-
-        const response = await request.post('users').send(createUserPayload);
-        expect(response.statusCode).to.equal(201);
-        console.log(JSON.stringify(response.body));
+    userdata.forEach((user: { name: string, job: string }) => {
+        it(`should validate create user ${user.name}`, async () => {
+            const response = await request.post('users').send(user);
+            expect(response.statusCode).to.equal(201);
+        })
     })
 
 })
