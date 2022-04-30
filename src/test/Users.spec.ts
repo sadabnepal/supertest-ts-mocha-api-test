73fc91fc99b20697a6669ee30099c4fbd0954102
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-04-30 07:05:13 +0530

    Response time and schema validation

--- src/test/Users.spec.ts
+++ src/test/Users.spec.ts
@@ -1,15 +1,23 @@
 import { expect } from 'chai';
+import { validate } from "jsonschema";
+import { performance } from 'perf_hooks';
 import { request } from 'src/config/supertest';
-import userdata from "src/resources/testdata.json";
+import users from "src/resources/testdata.json";
+import Userschema from 'src/resources/userschema.json';
 
 type User = { name: string, job: string };
 
 describe('Test ReqRes APIs', () => {
 
-    userdata.forEach((user: User) => {
+    users.forEach((user: User) => {
         it(`should validate create user ${user.name}`, async () => {
+            const startTime = performance.now();
             const response = await request.post('users').send(user);
+            expect(performance.now() - startTime, "Response time exceeded").to.be.lessThan(2000);
             expect(response.statusCode).to.equal(201);
+            expect(response.type).to.equal("application/json");
+            expect(validate(response.body, Userschema).valid).to.be.true;
+            expect(validate(response.body, Userschema).errors.length).to.equal(0);
         })
     })
 
