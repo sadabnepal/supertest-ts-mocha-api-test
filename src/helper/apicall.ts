213ca54595fe0d3a5217934a020aa6b1e9dcce6a
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-05-02 22:06:42 +0530

    sonarlint error fix

--- src/helper/apicall.ts
+++ src/helper/apicall.ts
@@ -2,11 +2,11 @@ import { request } from "src/config/supertest";
 import { endpoint } from "src/services/endpoints";
 import { Response } from "supertest";
 
-export const postCall = async (endpoint: endpoint, payload: object): Promise<Response> => {
-    return await request.post(endpoint).send(payload);
+export const postCall = async (service: endpoint, payload: object): Promise<Response> => {
+    return request.post(service).send(payload);
 }
 
-export const getCall = async (endpoint: endpoint, payload?: object): Promise<Response> => {
-    if (payload) return await request.get(endpoint).send(payload);
-    else return await request.get(endpoint);
+export const getCall = async (service: endpoint, payload?: object): Promise<Response> => {
+    if (payload) return request.get(service).send(payload);
+    else return request.get(service);
 }
