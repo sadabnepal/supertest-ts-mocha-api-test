52bd6414a1596fdd006cec5e7c5ca0d40930761f
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-05-03 19:38:57 +0530

    added headers as option parameter

--- src/helper/apicall.ts
+++ src/helper/apicall.ts
@@ -2,11 +2,13 @@ import { request } from "src/config/supertest";
 import { endpoint } from "src/services/endpoints";
 import { Response } from "supertest";
 
-export const postCall = async (service: endpoint, payload: object): Promise<Response> => {
+export const postCall = async (service: endpoint, payload: object, headers?: object): Promise<Response> => {
+    if (headers) return request.post(service).set(headers);
     return request.post(service).send(payload);
 }
 
-export const getCall = async (service: endpoint, payload?: object): Promise<Response> => {
-    if (payload) return request.get(service).send(payload);
+export const getCall = async (service: endpoint, payload?: object, headers?: object): Promise<Response> => {
+    if (payload && headers) return request.get(service).set(headers).send(payload);
+    else if (payload) return request.get(service).send(payload);
     else return request.get(service);
 }
