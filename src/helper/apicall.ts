b3aafcbcfb7fe8903bb96f78adada3b58ab5d484
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-05-22 09:28:23 +0530

    optimized undefined handeling

--- src/helper/apicall.ts
+++ src/helper/apicall.ts
@@ -3,14 +3,22 @@ import { logResponseToReport } from "@Helper/logger";
 import { GetApiType, PostApiType } from "@Types/http";
 import { Response } from "supertest";
 
+const setObjectOrEmptyIfUndefined = (data?: object) => data ? data : {};
+
 export const httpPostCall = async (post: PostApiType): Promise<Response> => {
-    let response = await request.post(post.service).set(post.headers ? post.headers : {}).send(post.payload);
+    let response = await request
+        .post(post.service)
+        .set(setObjectOrEmptyIfUndefined(post.headers))
+        .send(post.payload);
     logResponseToReport(post.context, response);
     return response;
 }
 
 export const httpGetCall = async (get: GetApiType): Promise<Response> => {
-    let response = await request.get(get.service).set(get.headers ? get.headers : {}).send(get.payload ? get.payload : {});
+    let response = await request
+        .get(get.service)
+        .set(setObjectOrEmptyIfUndefined(get.headers))
+        .send(setObjectOrEmptyIfUndefined(get.payload));
     logResponseToReport(get.context, response);
     return response;
 }
